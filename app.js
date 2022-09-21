844998659cd1f62c211c35ef6f84f9f07872a805
xAkashax <kbilan1992@gmail.com>
2022-09-21 15:31:32 +0200

    Add hover efects and animation for slider, navigation for desktop version

--- app.js
+++ app.js
@@ -2,27 +2,28 @@
 
 // pobieranie i zmiana zdjęć w tle
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   this.slider();
-// });
+document.addEventListener('DOMContentLoaded', () => {
+  this.slider();
+});
 
-// const bgImages = document.querySelector('.bg-img');
-// const howManyImages = 12;
-// let currentImg = 1;
+const bgImages = document.querySelector('.bg-img');
+const howManyImages = 10;
+let currentImg = 1;
 
-// function slider() {
-//   const version = window.innerWidth > 1024 ? 'desktop' : 'mobile';
-//   console.log(window.innerWidth);
+function slider() {
+  const version = window.innerWidth > 1024 ? 'desktop' : 'mobile';
+  console.log(window.innerWidth);
 
-//   if (currentImg > howManyImages) {
-//     currentImg = 1;
-//   }
-//   bgImages.style.backgroundImage = `url(img/${version}/${currentImg}.jpg)`;
-//   currentImg++;
-//   setTimeout(() => {
-//     this.slider();
-//   }, 1000);
-// }
+  if (currentImg > howManyImages) {
+    currentImg = 1;
+  }
+  bgImages.style.backgroundImage = `url(img/${version}/${currentImg}.jpg)`;
+  // bgImages.style.transition = 'opacity 1s ease-in-out';
+  currentImg++;
+  setTimeout(() => {
+    this.slider();
+  }, 5000);
+}
 
 // otwieranie menu
 
@@ -30,10 +31,18 @@ const openNav = document.querySelector('.open-nav');
 const closeNav = document.querySelector('.close-nav');
 const nav = document.querySelector('.nav-links');
 
+function activeNav() {
+  nav.classList.toggle('active-nav');
+}
+
 openNav.addEventListener('click', () => {
-  nav.style.display = 'block';
+  activeNav();
+  closeNav.style.display = 'block';
+  openNav.style.display = 'none';
 });
 
 closeNav.addEventListener('click', () => {
-  nav.style.display = 'none';
+  activeNav();
+  openNav.style.display = 'block';
+  closeNav.style.display = 'none';
 });
